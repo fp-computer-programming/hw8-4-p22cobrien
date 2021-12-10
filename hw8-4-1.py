40eb9da8e331fb8133f59e98389d68440a0fd764
p22cobrien <p22cobrien@fairifeldprep.org>
2021-12-09 19:12:45 -0500

    completed hw8-4-1.py

--- hw8-4-1.py
+++ hw8-4-1.py
@@ -2,4 +2,23 @@
 
 from random import randint
 
-computer = randint(0, 50)
+def num_game():
+    
+
+    while True:
+        player = input("Please enter a number between 0 & 50 ")
+        computer = randint(0, 50)
+
+        if player != "stop":
+            if int(player) == computer:
+                print("You guessed the number. The number was {0} and you geussed {1}".format(computer, player))
+                break
+            elif int(player) < computer:
+                print("You guessed wrong. The answer is higher. Try again.")
+            elif int(player) > computer:
+                print("You guessed wrong. The number is lower. Try again.")
+        elif player == "stop":
+            print("The number was {0}".format(computer))
+            break
+
+num_game()
